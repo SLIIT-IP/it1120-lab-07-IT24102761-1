02b14e7b67cae9e85bd1689a25b95432aa1d9d18
IT24102761 <it24102761@my.sliit.lk>
2024-09-20 17:43:27 +0530

    Add Lab7Q2A.java to Q2

---
+++ Q2/IT24102761Lab7Q2A.java
@@ -0,0 +1,15 @@
+public class IT24102761Lab7Q2A
+{
+    public static void main(String[]args)
+    {
+        int x,y;
+        for(x=1 ; x<=5 ; x++)
+        {
+            for(y=1 ; y<=5; y++)
+            {
+                System.out.print("$");
+            }
+            System.out.print("\n");
+        }
+    }
+}
